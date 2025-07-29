54bfef75e1bd009fa24b39f25ca1351326291479
Antonio Subašić <antonio.subasic.public@gmail.com>
2025-07-29 18:44:29 +0200

    refactor(config): build path regex during struct initialization
    improves performance by building regex only once during initialization and reusing it during runtime

--- src/config.rs
+++ src/config.rs
@@ -12,9 +12,30 @@ pub struct Config {
     pub cookie: Option<String>,
     #[serde(skip)]
     pub project_path: PathBuf,
+    #[serde(
+        skip_serializing,
+        skip_deserializing,
+        default = "Config::default_regex"
+    )]
+    path_regex: Regex,
 }
 
 impl Config {
+    // used by serde for default initialization of field
+    fn default_regex() -> Regex {
+        Regex::new("").unwrap()
+    }
+
+    // helper function
+    fn build_param_regex(param: &str, paddable: bool) -> Regex {
+        Regex::new(&format!(
+            r"\{{\{{\s*{}{}\s*\}}\}}",
+            if paddable { r"(pad\s+)?" } else { "" },
+            param
+        ))
+        .unwrap()
+    }
+
     pub fn load() -> Result<Self> {
         let home = dirs::home_dir().context("could not determine home directory")?;
 
@@ -29,68 +50,60 @@ impl Config {
             config.template_path = home.join(stripped).to_string_lossy().to_string();
         }
 
-        Ok(config)
-    }
+        // algorithm to escape template path and insert regex patterns for parameter extraction
+        {
+            let mut param_locations: Vec<Range<usize>> = Args::iter_names()
+                .flat_map(|(name, paddable)| {
+                    let re = Config::build_param_regex(name, paddable);
+                    re.find_iter(&config.template_path)
+                        .map(|m| m.start()..m.end())
+                        .collect::<Vec<Range<usize>>>()
+                })
+                .collect();
 
-    fn build_param_regex(param: &str, paddable: bool) -> Regex {
-        Regex::new(&format!(
-            r"\{{\{{\s*{}{}\s*\}}\}}",
-            if paddable { r"(pad\s+)?" } else { "" },
-            param
-        ))
-        .unwrap()
-    }
+            param_locations.sort_by_key(|range| range.start);
 
-    fn build_path_regex(&self) -> Result<Regex> {
-        let mut param_locations: Vec<Range<usize>> = Args::iter_names()
-            .flat_map(|(name, paddable)| {
-                let re = Config::build_param_regex(name, paddable);
-                re.find_iter(&self.template_path)
-                    .map(|m| m.start()..m.end())
-                    .collect::<Vec<Range<usize>>>()
-            })
-            .collect();
+            let mut pattern = String::new();
+            let mut previous = 0;
 
-        param_locations.sort_by_key(|range| range.start);
+            for range in &param_locations {
+                if previous < range.start {
+                    pattern.push_str(&regex::escape(&config.template_path[previous..range.start]));
+                }
 
-        let mut pattern = String::new();
-        let mut previous = 0;
-
-        for range in &param_locations {
-            if previous < range.start {
-                pattern.push_str(&regex::escape(&self.template_path[previous..range.start]));
+                pattern.push_str(&config.template_path[range.clone()]);
+                previous = range.end;
             }
 
-            pattern.push_str(&self.template_path[range.clone()]);
-            previous = range.end;
+            if previous < config.template_path.len() {
+                pattern.push_str(&regex::escape(&config.template_path[previous..]));
+            }
+
+            let pattern = pattern
+                .replace("{{year}}", r"(?P<year>\d{4})")
+                .replace("{{day}}", r"(?P<day>[1-9]|1[0-9]|2[0-5])")
+                .replace("{{pad day}}", r"(?P<padday>0[1-9]|1[0-9]|2[0-5])")
+                .replace(
+                    "{{language}}",
+                    &format!(
+                        r"(?P<language>{})",
+                        Language::iter()
+                            .map(|l| l.to_string())
+                            .collect::<Vec<String>>()
+                            .join("|")
+                    ),
+                );
+
+            config.path_regex = Regex::new(&pattern)?;
         }
 
-        if previous < self.template_path.len() {
-            pattern.push_str(&regex::escape(&self.template_path[previous..]));
-        }
-
-        let pattern = pattern
-            .replace("{{year}}", r"(?P<year>\d{4})")
-            .replace("{{day}}", r"(?P<day>[1-9]|1[0-9]|2[0-5])")
-            .replace("{{pad day}}", r"(?P<padday>0[1-9]|1[0-9]|2[0-5])")
-            .replace(
-                "{{language}}",
-                &format!(
-                    r"(?P<language>{})",
-                    Language::iter()
-                        .map(|l| l.to_string())
-                        .collect::<Vec<String>>()
-                        .join("|")
-                ),
-            );
-
-        Ok(Regex::new(&pattern)?)
+        Ok(config)
     }
 
     pub fn build(&mut self, args: &mut Args) -> Result<()> {
         let working_directory = env::current_dir()?.to_string_lossy().into_owned();
 
-        if let Some(captures) = self.build_path_regex()?.captures(&working_directory) {
+        if let Some(captures) = self.path_regex.captures(&working_directory) {
             if captures.len() == 4
                 && captures.name("year").is_some()
                 && (captures.name("day").is_some() || captures.name("padday").is_some())
